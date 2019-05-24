2552551c6411da40f9ac42e96e24c4b9bd29736a
Constanza De Rienzo <cderienzo@itba.edu.ar>
2019-05-24 17:07:02 -0300

    gene mutation

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -127,7 +127,9 @@ def gene_mutation(offspring_crossover, mutation_prob):
     for idx in range(offspring_crossover.shape[0]):
         # Perform mutation on individual if random number < mutation_probability
         if(numpy.random.random_sample() < mutation_prob):
-            random_value = numpy.random.uniform(-1.0, 1.0, 1)
+            random_value = numpy.random.randint(0, offspring_crossover.shape[1])
+            mutation_value = numpy.random.randint(0,9)
+            offspring_crossover[idx][random_value] = mutation_value
     return offspring_crossover
 
 def multigene_mutation(offspring_crossover, mutation_prob):
