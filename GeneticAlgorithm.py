cfef58678edf0bc53256a9a96d4707af29cb361c
Agustina Osimani <agustina.osimani@zoomagri.com>
2019-05-25 18:25:34 -0300

    FIX items

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -5,11 +5,7 @@ from Config import Config
 from Loader import Loader
 
 class GeneticAlgorithm():
-    def __init__(self):
-        self.items = Items(Config.files)
-        self.items_count = len(Config.files)
-        self.item_count = self.items.item_count()
-
+    
     # Create an initial seed population
     def seed(self):
         population = list()
@@ -30,11 +26,11 @@ class GeneticAlgorithm():
     # Calculating the fitness value of each solution in the current population.   
         fitness_vector = list()
         for individual in population:
-            arms = self.items.item_value(individual['items'][0], 0)
-            boots = self.items.item_value(individual['items'][1], 1)
-            helmets = self.items.item_value(individual['items'][2], 2)
-            gloves = self.items.item_value(individual['items'][3], 3)
-            vests = self.items.item_value(individual['items'][4], 4)
+            arms = Loader.item_value(individual['items'][0], 0)
+            boots = Loader.item_value(individual['items'][1], 1)
+            helmets = Loader.item_value(individual['items'][2], 2)
+            gloves = Loader.item_value(individual['items'][3], 3)
+            vests = Loader.item_value(individual['items'][4], 4)
             strength = arms[0] + boots[0] + helmets[0] + gloves[0] + vests[0]
             agility = arms[1] + boots[1] + helmets[1] + gloves[1] + vests[1]
             skill = arms[2] + boots[2] + helmets[2] + gloves[2] + vests[2]
