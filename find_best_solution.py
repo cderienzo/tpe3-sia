def30607afe193280ceb56c387a31760b9ec9b6a
Juan Godfrid <juan.godfrid@gmail.com>
2019-05-25 18:28:14 -0300

    a

--- find_best_solution.py
+++ find_best_solution.py
@@ -11,16 +11,9 @@ for generation in range(Config.num_generations):
     print("Generation : %d" % generation)
     population = GA.replacement(population)
 
-
-# Getting the best solution after iterating finishing all generations.
-#At first, the fitness is calculated for each solution in the final generation.
 fitness = GA.fitness(population)
-
-# Then return the index of that solution corresponding to the best fitness.
 best_match_idx = numpy.where(fitness == numpy.max(fitness))
-
 index = best_match_idx[0][0]
 
 print("Best solution : ", population[index])
-
 print("Best solution fitness : ", fitness[index])
