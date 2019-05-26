467af3279e9d9aed8728afaf0f64cc4c301d24fa
Juan Godfrid <juan.godfrid@gmail.com>
2019-05-25 23:33:04 -0300

    replacement

--- replacement.py
+++ replacement.py
@@ -1,38 +1,42 @@
 def one(GA, population):
     fitness = GA.fitness(population)
-    parents = GA.select_mating_pool(population, fitness)
 
-    offspring_crossover = GA.crossover(parents)
+    offspring = []
 
-    return GA.mutation(offspring_crossover)  
+    while(len(offspring)<len(population)):
+        parents = random.sample(population, 2)
+        offspring.append(GA.crossover(parents[0], parents[1]))
+    
+    offspring = GA.mutation(offspring)
+    return offspring
 
 def two(GA, population):
     fitness = GA.fitness(population)
     parents = GA.select_mating_pool(population, fitness)
 
-    return None    
+    offspring = []
+
+    while(len(offspring)<len(parents)):
+        random_parents = random.sample(parents, 2)
+        offspring.append(GA.crossover(random_parents[0], random_parents[1]))
+
+    offspring = GA.mutation(offspring)
+
+    offspring.append(random.sample(population,len(population)-Config.num_parents_mating))
+    return offspring    
 
 def three(GA, population):
-    # TODO
-    return None
-
     fitness = GA.fitness(population)
     parents = GA.select_mating_pool(population, fitness)
 
-    offspring_crossover = GA.crossover(parents)
-    offspring_mutation = GA.mutation(offspring_crossover)
+    offspring = []
 
-    # Select which ones make it to the new generation
-    total_pool = numpy.append(parents, offspring_mutation,axis=0)
-    fitness = GA.fitness(total_pool)
-    next_generation = GA.select_mating_pool(total_pool, fitness)
-    new_population = next_generation
-    # Creating the new population based on the parents and offspring.
-    #new_population[0:parents.shape[0], :] = parents
-    #new_population[parents.shape[0]:, :] = offspring_mutation
+    while(len(offspring)<len(parents)):
+        random_parents = random.sample(parents, 2)
+        offspring.append(GA.crossover(random_parents[0], random_parents[1]))
 
-    fitness = GA.fitness(new_population)
-    # The best result in the current iteration.
-    best_match_idx = numpy.where(fitness == numpy.max(fitness))
-    index = best_match_idx[0][0]
-    print("Best solution : ", new_population[index])   
+    offspring = GA.mutation(offspring)
+
+    new_pop = random.sample(population,len(population)-Config.num_parents_mating)
+
+    return new_pop.extend(random.sample(population.extend(offspring), Config.num_parents_mating))
