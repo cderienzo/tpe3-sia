99943e3fab901c8ff1576a37c98bc9fb4cd95a9c
Juan Godfrid <juan.godfrid@gmail.com>
2019-05-26 13:27:32 -0300

    items count

--- mutation.py
+++ mutation.py
@@ -5,12 +5,12 @@ import items
 def gene(offspring_crossover):
     for offspring in offspring_crossover:
         if(numpy.random.random_sample() < Config.mutation_prob):
-            random_value = numpy.random.randint(0, len(offspring['items'])+1)
-            if random_value == len(offspring_crossover['items']):
+            random_value = numpy.random.randint(0, items.items_count()+1)
+            if(random_value == items.items_count()):
                 mutation_value = numpy.random.uniform(Config.min_height, Config.max_height)
                 offspring['height'] = mutation_value
             else:
-                mutation_value = numpy.random.random_integers(0,items.items_count)
+                mutation_value = numpy.random.random_integers(0,items.items_count())
                 offspring['items'][random_value] = mutation_value 
     return offspring_crossover
 
@@ -18,7 +18,7 @@ def multigene(offspring_crossover):
     for offspring in offspring_crossover:
         for gene in offspring['items']:
             if(numpy.random.random_sample() < Config.mutation_prob):                
-                mutation_value = numpy.random.random_integers(0,items.items_count)
+                mutation_value = numpy.random.random_integers(0,items.items_count())
                 gene = mutation_value
         if(numpy.random.random_sample() < Config.mutation_prob):
             offspring['height'] = numpy.random.uniform(Config.min_height, Config.max_height)
