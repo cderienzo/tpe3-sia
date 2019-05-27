4de4c1d05d1954b31bd4adb54b277418e5b82781
Juan Godfrid <juan.godfrid@gmail.com>
2019-05-27 16:49:17 -0300

    replacement 1 fix

--- replacement.py
+++ replacement.py
@@ -4,7 +4,6 @@ from Config import Config
 def one(GA, population, fitness):
     offspring = []
 
-    #todo restarselo a pop asi no se repite
     while(len(population)!=0):
         parents = random.sample(population, 2)
         offspring.extend(GA.crossover(parents[0], parents[1]))
@@ -17,7 +16,7 @@ def one(GA, population, fitness):
 def two(GA, population, fitness):
     # TODO buscar mejor manera sin hacer backup
     population_backup = population.copy
-    parents = GA.select_mating_pool(population, fitness, 'A', Config.num_parents_mating)
+    parents = GA.select_mating_pool(population, fitness, 'A', GA,Config.num_parents_mating)
 
     offspring = []
 
@@ -30,13 +29,13 @@ def two(GA, population, fitness):
 
     offspring = GA.mutation(offspring)
 
-    offspring.extend(GA.select_mating_pool(population_backup, fitness, 'B', len(population_backup)-Config.num_parents_mating))
+    offspring.extend(GA.select_mating_pool(population_backup, fitness, 'B', GA, len(population_backup)-Config.num_parents_mating))
     return offspring    
 
 def three(GA, population, fitness):
     # TODO buscar mejor manera sin hacer backup
     population_backup = population.copy
-    parents = GA.select_mating_pool(population, fitness, 'A', Config.num_parents_mating)
+    parents = GA.select_mating_pool(population, fitness, 'A', GA, Config.num_parents_mating)
 
     offspring = []
 
@@ -48,7 +47,7 @@ def three(GA, population, fitness):
 
     offspring = GA.mutation(offspring)
 
-    new_pop = GA.select_mating_pool(population_backup, fitness, 'B', len(population_backup)-Config.num_parents_mating)
+    new_pop = GA.select_mating_pool(population_backup, fitness, 'B', GA, len(population_backup)-Config.num_parents_mating)
     population_backup.extend(offspring)
-    new_pop.extend(GA.select_mating_pool(population_backup, fitness, 'B', Config.num_parents_mating))
+    new_pop.extend(GA.select_mating_pool(population_backup, fitness, 'B', GA, Config.num_parents_mating))
     return new_pop
