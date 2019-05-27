8de79f0938fb1bbd8a6d6b8c1b0bf8f8185647bb
Agustina Osimani <agustina.osimani@zoomagri.com>
2019-05-26 21:05:02 -0300

    optimization item_count()

--- items.py
+++ items.py
@@ -1,6 +1,7 @@
 import pandas
 
 files = ['testdata/armas.tsv', 'testdata/botas.tsv', 'testdata/cascos.tsv', 'testdata/guantes.tsv', 'testdata/pecheras.tsv']                 
+item_count_global = 0
 
 def item_value(id, item_id):
     return pandas.read_csv(files[item_id], names=['id', 'strength', 'agility', 'skill', 'resistence', 'life'], skiprows=id+1, nrows=1, delimiter='\t')
@@ -9,5 +10,8 @@ def items_count():
     return len(files)        
 
 def item_count():
-    with open('testdata/armas.tsv') as item:
-        return sum(1 for line in item) - 2
+    global item_count_global
+    if (item_count_global == 0):
+        with open(files[0]) as item:
+                item_count_global = sum(1 for line in item) - 2            
+    return item_count_global
