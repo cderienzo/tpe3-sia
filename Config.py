1ac95fc5563cb908ebee81d4404061aff2ffd816
Agustina Osimani <agustina.osimani@zoomagri.com>
2019-05-28 18:20:34 -0300

    opciones characters

--- Config.py
+++ Config.py
@@ -1,5 +1,6 @@
 class Config:
 
+    # Las opciones pueden ser 'Defensor', 'Guerrero', 'Arquero', o 'Asesino'
     character = 'Defensor'
 
     character_num = 2
@@ -41,8 +42,10 @@ class Config:
 
     # 1 = Exponential Multiplicative, 2 = Logarithmical Multiplicative, 3 = Linear Multiplicative, 4 = Linear Additive
     cooling_schedule = 4
+    
     # For schedule 1: alpha between 0.8 and 0.9, for schedule 2: alpha > 1, for schedule 3: alpha > 0 
     cooling_alpha = 1.5
+    
     # Rounds for tournament selection method
     tournament_rounds = 3
 
